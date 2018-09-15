ddfeb730a460a1d8a3763b7d80349b7784df9a1e
Kailas Moon <kailasmoon@gmail.com>
2018-09-15 16:00:13 -0400

    lol

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000
 
+var user = 1;
+var volumes = [];
+
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -14,16 +17,30 @@ app.get('/',function(req,res){
   res.sendFile(__dirname+'/public/index.html');
 });
 
+app.get('/beacons',function(req,res){
+  res.sendFile(__dirname+'/public/beacons.html');
+});
+
+app.get('/getusername', function(req, res) {
+  res.send('' + user);
+  user++;
+});
+
 app.get('/timetest',function(req,res){
   res.sendFile(__dirname+'/public/timetest.html');
 });
 
 app.post('/time', function(req, res) {
   console.log(req.body.sent - Date.now());
-})
+});
 
-app.post('/uploadImage', function(req,res) {
-  console.log(req.data);
+app.post('/update', function(req,res) {
+  if (volumes.length < 3) {
+    volumes.push(req.body.volume);
+  } else {
+    
+    volumes = [];
+  }
 });
 
 
